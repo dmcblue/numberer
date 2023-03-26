568dd4a3ea115bdf656b5932898e6b1905523a1e
dmcblue <dmcblue@gmail.com>
2023-03-26 16:22:32 -0400

    Move logic in to js file

--- index.php
+++ index.php
@@ -21,8 +21,7 @@
 		<link rel="canonical" href="<?= $baseUrl ?>/index.html" />
 		<meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"/>
 
-		<!-- <link href="<?= $baseUrl ?>/style.css" rel="stylesheet" type="text/css">
-		<link href="<?= $baseUrl ?>/home.css" rel="stylesheet" type="text/css"> -->
+		<link href="<?= $baseUrl ?>/styles.css" rel="stylesheet" type="text/css">
 		<!-- Fonts -->
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -58,137 +57,68 @@
 			<script type='application/ld+json'>{"@context":"https:\/\/schema.org","@graph":[{"@type":"WebSite","@id":"<?= json_encode($baseUrl) ?>","url":"<?= json_encode($baseUrl) ?>","name":"dmcblue","publisher":{"@id":"<?= json_encode($baseUrl) ?>\/about"}},{"@type":"ImageObject","@id":"<?= json_encode($baseUrl) ?>\/opengraph.png","url":"<?= json_encode($baseUrl) ?>\/opengraph.png","width":1273,"height":775},{"@type":"WebPage","@id":"<?= json_encode($baseUrl) ?>\/","url":"<?= json_encode($baseUrl) ?>\/","inLanguage":"en-US","name":"dmcblue","isPartOf":{"@id":"<?= json_encode($baseUrl) ?>"},"primaryImageOfPage":{"@id":"<?= json_encode($baseUrl) ?>\/opengraph.png"},"datePublished":null,"dateModified":null},{"@type":"Article","@id":"<?= json_encode($baseUrl) ?>\/","isPartOf":{"@id":"<?= json_encode($baseUrl) ?>\/"},"author":{"@id":"<?= json_encode($baseUrl) ?>\/about"},"headline":"dmcblue","datePublished":null,"dateModified":null,"commentCount":0,"mainEntityOfPage":{"@id":"<?= json_encode($baseUrl) ?>\/"},"publisher":{"@id":"<?= json_encode($baseUrl) ?>\/about"},"image":{"@id":"<?= json_encode($baseUrl) ?>\/opengraph.png"},"keywords":"","articleSection":""},{"@type":["Person"],"@id":"<?= json_encode($baseUrl) ?>\/about","name":"dmcblue","image":{"@type":"ImageObject","@id":"\/opengraph.png","url":"\/opengraph.png","caption":"dmcblue"},"sameAs":[]}]}</script>
 			<!-- END Json LD -->
 		<!-- END Metadata -->
-		<style>
-			body {
-				font-family: 'Vazirmatn', sans-serif;
-				background: rgba(150, 150, 255, 1);
-				text-align: center;
-			}
-			#disp, #answer {
-				min-height: 4rem;
-				margin-top: 1rem;
-			}
-			#disp .step, #answer .step {
-				margin-right: 0.5rem;
-			}
-			#disp, #answer[lang=en] {
-				font-size: 400%;
-			}
-			#disp[lang=en], #answer[lang=en] {
-				font-size: 300%;
-			}
-			#answer {
-				margin-bottom: 1rem;
-			}
-			#answer .step {
-				border-bottom: 1px solid black;
-			}
-			.step {
-				display: inline-block;
-				width: 2.5rem;
-				text-align: center;
-			}
-			.step.correct {
-				background: rgba(10, 200, 10, 1);
-			}
-			.step.incorrect {
-				background: rgba(200, 10, 10, 1);
-			}
-			select, button {
-				border: none;
-				background: rgba(10, 10, 200, 0.5);
-			}
-			select {
-				margin-right: 1em;
-			}
-			table {
-				margin: 0 auto;
-			}
-			table button {
-				width: 2.5rem;
-				padding: 0.5rem;
-				font-size: 150%;
-			}
-			table button {
-				font-size: 250%;
-			}
-			table[lang=en] button {
-				font-size: 250%;
-			}
-			table[lang=fa] button {
-				font-size: 250%;
-			}
-			table[lang=bn] button {
-				font-size: 250%;
-			}
-			#flip {
-				margin-top: 1em;
-			}
-			p {
-				font-size: 80%;
-			}
-		</style>
+
 		<script src="numberer.js" async="" defer="" crossorigin="anonymous"></script>
 	</head>
-	<body onload="start()" onkeyup="onKeyUp(event)">
+	<body onload="start()" onkeyup="UI.onKeyUp(event)">
 		<h1>Learn Numbers</h1>
-		From: <select id="source" onchange="setLanguage()">
+		From: <select id="source" onchange="UI.setLanguage()">
 			<option value="ar">Arabic</option>
 			<option value="bn">Bengali</option>
 			<option value="fa">Farsi</option>
 			<option value="ur">Urdu</option>
 			<option value="en">English</option>
 		</select>
-		To: <select id="target" onchange="setLanguage()">
-			<option value="en">English</option>
+		To: <select id="target" onchange="UI.setLanguage()">
 			<option value="ar">Arabic</option>
 			<option value="bn">Bengali</option>
+			<option value="en">English</option>
 			<option value="fa">Farsi</option>
 			<option value="ur">Urdu</option>
 		</select>
 		<br/>
-		<button id="flip" onclick="flip()">Switch 🔄</button>
+		<button id="flip" onclick="UI.flip()">Switch 🔄</button>
 		<div id="disp"></div>
 		<div id="answer"></div>
 		<table id="numpad">
 			<tbody>
 				<tr>
 					<td>
-						<button id="b7" onclick="test(7)"></button>
+						<button id="b7" onclick="UI.test(7)"></button>
 					</td>
 					<td>
-						<button id="b8" onclick="test(8)"></button>
+						<button id="b8" onclick="UI.test(8)"></button>
 					</td>
 					<td>
-						<button id="b9" onclick="test(9)"></button>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<button id="b4" onclick="test(4)"></button>
-					</td>
-					<td>
-						<button id="b5" onclick="test(5)"></button>
-					</td>
-					<td>
-						<button id="b6" onclick="test(6)"></button>
+						<button id="b9" onclick="UI.test(9)"></button>
 					</td>
 				</tr>
 				<tr>
 					<td>
-						<button id="b1" onclick="test(1)"></button>
+						<button id="b4" onclick="UI.test(4)"></button>
 					</td>
 					<td>
-						<button id="b2" onclick="test(2)"></button>
+						<button id="b5" onclick="UI.test(5)"></button>
 					</td>
 					<td>
-						<button id="b3" onclick="test(3)"></button>
+						<button id="b6" onclick="UI.test(6)"></button>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<button id="b1" onclick="UI.test(1)"></button>
+					</td>
+					<td>
+						<button id="b2" onclick="UI.test(2)"></button>
+					</td>
+					<td>
+						<button id="b3" onclick="UI.test(3)"></button>
 					</td>
 				</tr>
 				<tr>
 					<td></td>
 					<td>
-						<button id="b0" onclick="test(0)"></button>
+						<button id="b0" onclick="UI.test(0)"></button>
 					</td>
 					<td></td>
 				</tr>
@@ -198,121 +128,8 @@
 			Click or type to enter numbers.
 		</p>
 		<script>
-			const possible = ['ar', 'fa', 'ur', 'en', 'bn'];
-			let source = 'ar';
-			let target = 'en';
-			let current = [0];
-			let testIndex = 0;
 			function start() {
-				const queryString = window.location.search;
-				const urlParams = new URLSearchParams(queryString);
-				if (possible.includes(urlParams.get('source'))) {
-					source = urlParams.get('source');
-				}
-				if (possible.includes(urlParams.get('target'))) {
-					target = urlParams.get('target');
-				}
-				document.getElementById('source').value = source;
-				document.getElementById('target').value = target;
-				setLanguage();
-				fillButtons();
-				next();
-			}
-
-			function setLanguage() {
-				source = document.getElementById('source').value;
-				target = document.getElementById('target').value;
-				const queryString = window.location.search;
-				const urlParams = new URLSearchParams(queryString);
-				urlParams.set('source', source);
-				urlParams.set('target', target);
-				var newRelativePathQuery = window.location.pathname + '?' + urlParams.toString();
-    			history.pushState(null, '', newRelativePathQuery);
-				fillButtons();
-				next();
-			}
-
-			function flip() {
-				document.getElementById('source').value = target;
-				document.getElementById('target').value = source;
-				setLanguage();
-			}
-
-			function onKeyUp(event) {
-				const keyCode = parseInt(event.keyCode);
-				if (keyCode > 47 && keyCode < 58) {
-					test(keyCode - 48);
-				} else if (keyCode > 95 && keyCode < 106) {
-					test(keyCode - 96);
-				}
-			}
-
-			function getDigitsInLanguage(lang, digits) {
-				if (lang === 'en') {
-					return digits;
-				} else if (lang === 'ar') {
-					return digitsToArabic(digits);
-				} else if (lang === 'bn') {
-					return digitsToBengali(digits);
-				}
-
-				// fa, ur
-				return digitsToFarsi(digits);
-			}
-
-			function next() {
-				let numDigits = getRandomInteger(1, 5);
-				const disp = document.getElementById('disp');
-				disp.lang = source;
-				let temp = getDigits(numDigits);
-				while (temp.join('') === current.join('') || (temp[0] === 0 && temp.length > 1)) {
-					temp = getDigits(numDigits);
-				}
-				current = temp;
-				disp.innerHTML = '';
-				const lDigits = getDigitsInLanguage(source, current);
-				for (let i = 0; i < current.length; i++) {
-					const step = document.createElement('div');
-					step.className = 'step';
-					step.textContent = lDigits[i];
-					disp.appendChild(step);
-				}
-				testIndex = 0;
-
-				const answer = document.getElementById('answer');
-				answer.innerHTML = '';
-				answer.lang = target;
-				for (let i = 0; i < current.length; i++) {
-					const step = document.createElement('div');
-					step.className = 'step';
-					step.id = `s${i}`;
-					step.innerHTML = '&nbsp;';
-					answer.appendChild(step);
-				}
-			}
-
-			function fillButtons() {
-				document.getElementById('numpad').lang = target;
-				for (let i = 0; i < 10; i++) {
-					const n = getDigitsInLanguage(target, [i])[0];
-					document.getElementById(`b${i}`).textContent = n;
-				}
-			}
-
-			function test(n) {
-				const step = document.getElementById(`s${testIndex}`);
-				step.textContent = getDigitsInLanguage(target, [n])[0];
-				if (n === current[testIndex]) {
-					step.className = 'step correct';
-					testIndex++;
-					if (testIndex === current.length) {
-						setTimeout(function() {
-							next();
-						}, 500);
-					}
-				} else {
-					step.className = 'step incorrect';
-				}
+				UI.start();
 			}
 		</script>
 	</body>
--- public/numberer.js
+++ public/numberer.js
@@ -1,41 +1,163 @@
-// const s1 = '\u00E9'
 // https://en.wikipedia.org/wiki/Eastern_Arabic_numerals
 // Arabic: U+0660 through U+0669, 1632
 // Farsi:  U+06F0 through U+06F9, 1776
 // Urdu:   Same Unicode characters as the Persian, but language is set to Urdu. The numerals 4, 6 and 7 are different from Persian. On some devices, this row may appear identical to Persian.
-
-function getDigit() {
+// https://en.wikipedia.org/wiki/Bengali_numerals
+const Tools = {};
+Tools.getDigit = function () {
 	return Math.floor(Math.random() * 10);
-}
+};
 
-
-function getRandomInteger(min, max) {
+Tools.getRandomInteger = function (min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 
-function getDigits(n) {
+Tools.getDigits = function getDigits(n) {
 	let digits = [];
 	for (let i = 0; i < n; i++) {
-		digits.push(getDigit());
+		digits.push(Tools.getDigit());
 	}
 
 	return digits;
-}
+};
 
-function digitsToArabic(digits) {
+Tools.digitsToArabic = function (digits) {
 	return digits.map(function(digit) {
 		return String.fromCodePoint(1632 + digit);
 	});
-}
+};
 
-function digitsToFarsi(digits) {
-	return digits.map(function(digit) {
-		return String.fromCodePoint(1776 + digit);
-	});
-}
-
-function digitsToBengali(digits) {
+Tools.digitsToBengali = function (digits) {
 	return digits.map(function(digit) {
 		return String.fromCodePoint(2534 + digit);
 	});
+};
+
+Tools.digitsToFarsi = function (digits) {
+	return digits.map(function(digit) {
+		return String.fromCodePoint(1776 + digit);
+	});
+};
+
+Tools.getDigitsInLanguage = function (lang, digits) {
+	if (lang === 'en') {
+		return digits;
+	} else if (lang === 'ar') {
+		return Tools.digitsToArabic(digits);
+	} else if (lang === 'bn') {
+		return Tools.digitsToBengali(digits);
+	}
+
+	// fa, ur
+	return Tools.digitsToFarsi(digits);
 }
+
+Object.freeze(Tools);
+
+const UI = {};
+UI.possibleLanguages = Object.freeze(['ar', 'fa', 'ur', 'en', 'bn']);
+UI.state = {
+	source: 'ar',
+	target: 'en',
+	current: [0], // the current test number
+	testIndex: 0
+};
+
+UI.setLanguage = function () {
+	UI.state.source = document.getElementById('source').value;
+	UI.state.target = document.getElementById('target').value;
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	urlParams.set('source', UI.state.source);
+	urlParams.set('target', UI.state.target);
+	var newRelativePathQuery = window.location.pathname + '?' + urlParams.toString();
+	history.pushState(null, '', newRelativePathQuery);
+	UI.fillButtons();
+	UI.next();
+};
+
+UI.flip = function () {
+	document.getElementById('source').value = UI.state.target;
+	document.getElementById('target').value = UI.state.source;
+	UI.setLanguage();
+};
+
+UI.onKeyUp = function (event) {
+	const keyCode = parseInt(event.keyCode);
+	if (keyCode > 47 && keyCode < 58) {
+		UI.test(keyCode - 48);
+	} else if (keyCode > 95 && keyCode < 106) {
+		UI.test(keyCode - 96);
+	}
+};
+
+UI.next = function () {
+	let numDigits = Tools.getRandomInteger(1, 5);
+	const disp = document.getElementById('disp');
+	disp.lang = UI.state.source;
+	let temp = Tools.getDigits(numDigits);
+	while (temp.join('') === UI.state.current.join('') || (temp[0] === 0 && temp.length > 1)) {
+		temp = Tools.getDigits(numDigits);
+	}
+	UI.state.current = temp;
+	disp.innerHTML = '';
+	const lDigits = Tools.getDigitsInLanguage(UI.state.source, UI.state.current);
+	for (let i = 0; i < UI.state.current.length; i++) {
+		const step = document.createElement('div');
+		step.className = 'step';
+		step.textContent = lDigits[i];
+		disp.appendChild(step);
+	}
+	UI.state.testIndex = 0;
+
+	const answer = document.getElementById('answer');
+	answer.innerHTML = '';
+	answer.lang = UI.state.target;
+	for (let i = 0; i < UI.state.current.length; i++) {
+		const step = document.createElement('div');
+		step.className = 'step';
+		step.id = `s${i}`;
+		step.innerHTML = '&nbsp;';
+		answer.appendChild(step);
+	}
+};
+
+UI.fillButtons = function () {
+	document.getElementById('numpad').lang = UI.state.target;
+	for (let i = 0; i < 10; i++) {
+		const n = Tools.getDigitsInLanguage(UI.state.target, [i])[0];
+		document.getElementById(`b${i}`).textContent = n;
+	}
+};
+
+UI.test = function (n) {
+	const step = document.getElementById(`s${UI.state.testIndex}`);
+	step.textContent = Tools.getDigitsInLanguage(UI.state.target, [n])[0];
+	if (n === UI.state.current[UI.state.testIndex]) {
+		step.className = 'step correct';
+		UI.state.testIndex++;
+		if (UI.state.testIndex === UI.state.current.length) {
+			setTimeout(function() {
+				UI.next();
+			}, 500);
+		}
+	} else {
+		step.className = 'step incorrect';
+	}
+};
+
+UI.start = function () {
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	if (UI.possibleLanguages.includes(urlParams.get('source'))) {
+		UI.state.source = urlParams.get('source');
+	}
+	if (UI.possibleLanguages.includes(urlParams.get('target'))) {
+		UI.state.target = urlParams.get('target');
+	}
+	document.getElementById('source').value = UI.state.source;
+	document.getElementById('target').value = UI.state.target;
+	UI.setLanguage();
+	UI.fillButtons();
+	UI.next();
+};
